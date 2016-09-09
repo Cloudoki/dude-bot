475d93a59e6ad84cb028268e75dbb1ce75234fa6
Edgar Ribeiro <edgar.marques.ribeiro@gmail.com>
2016-09-09 17:17:38 +0100

    simplify recording

--- record.py
+++ record.py
@@ -1,13 +1,13 @@
 # http://stackoverflow.com/questions/20131689/raspberry-pi-convert-pyaudio-wav-to-flac-48000hz-google-speech
 
 import pyaudio
+import signal
+import sys
 
 chunk = 2048
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 16000
-THRESHOLD = 525 #The threshold intensity that defines silence signal (lower than).
-SILENCE_LIMIT = 3 #Silence limit in seconds. The max ammount of seconds where only silence is recorded. When this time passes the recording finishes and the file is delivered.
 
 #open stream
 p = pyaudio.PyAudio()
@@ -18,43 +18,35 @@ stream = p.open(format = FORMAT,
                 input = True,
                 frames_per_buffer = chunk)
 
-print "* listening. CTRL+C to finish manually."
 all_m = []
 data = ''
 rel = RATE/chunk
-slid_win = deque(maxlen=SILENCE_LIMIT*rel)
-started = False
-recording = True
-while (recording):
-    data = stream.read(chunk)
-    slid_win.append (abs(audioop.avg(data, 2)))
 
-    if(True in [ x>THRESHOLD for x in slid_win]):
-        if(not started):
-            print "starting record"
-        started = True
-        all_m.append(data)
-    elif (started==True):
-        print "finished"
-        #the limit was reached, finish capture and deliver
-        filename = save_speech(all_m, p)
-        started = False
-        #slid_win = deque(maxlen=SILENCE_LIMIT*rel)
-        #all_m= []
-        stream.close()
-        p.terminate()
-        recording = False
-
+def on_exit(signal, frame):
+    print('You pressed Ctrl+C!')
+    filename = save_speech(all_m, p)
+    stream.close()
+    p.terminate()
+    sys.exit(0)
 
 def save_speech(data, p):
     filename = 'file'
     # write data to WAVE file
     data = ''.join(data)
     wf = wave.open(filename+'.wav', 'wb')
-    wf.setnchannels(1)
+    wf.setnchannels(CHANNELS)
     wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
-    wf.setframerate(48000)
+    wf.setframerate(RATE)
     wf.writeframes(data)
     wf.close()
     print "finished saving wav: %s" % filename
     return filename
+
+signal.signal(signal.SIGINT, on_exit)
+
+while (True):
+    data = stream.read(chunk)
+    all_m.append(data)
+
+print "* listening. CTRL+C to finish manually."
+signal.pause()
