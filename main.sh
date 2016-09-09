89fb5db7848cede5a6eb46b250da6fafcd92b1ae
Edgar Ribeiro <edgar.marques.ribeiro@gmail.com>
2016-09-09 16:48:57 +0100

    improve ui

--- main.sh
+++ main.sh
@@ -5,9 +5,9 @@ echo "Recording… Press Ctrl+C to Stop."
 ./speech2text.sh
 
 QUESTION=$(cat stt.txt)
-echo "Me: ", "$QUESTION"
+echo "Me: $QUESTION"
 
 ANSWER=$(python queryprocess.py "$QUESTION")
-echo "Robot: ", "$ANSWER"
+echo "Robot: $ANSWER"
 
 python tts.py "$ANSWER"
